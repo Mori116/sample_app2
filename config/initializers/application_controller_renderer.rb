3a37720a014d065b0f97c2f962fc6c5567b42f36
Mori116 <cacaomame096@gmail.com>
2021-05-19 10:14:51 +0000

    [Finish]2-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '6ff440b8a844612d3b45c5b13088b3734bb673fdf4869df104f6aad4a6ebca99220c641bf0dc395bfd03ada3a3f9497a5c72ce21c904027f4c7b41f06cf6340b'
